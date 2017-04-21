1351cf43cc0d673ea0e0845b242cf1217f82eb56
Berkay_Dedeoglu <berkay.dedeoglu@yandex.com>
2017-04-21 20:50:55 +0300

    Proje Olusturuldu ve Python icin bazi siralama algoritmalari eklendi. Eklenen algoritmalar:
     -> Selection Sort
     -> Insertion Sort
     -> Bubble Sort
     -> Merge Sort (Tamamlanmadi)

---
+++ Python/MergeSorted.py
@@ -0,0 +1,22 @@
+from SortedList import SortedList
+
+
+class MergeSorted(SortedList):
+
+    def sort(self):
+        pass
+
+    def merge(self):
+        pass
+
+    def seperator(self):
+        pass
+
+if __name__ == "__main__":
+    """
+    obj = MergeSorted([2, 1, -2, 3, 1, 1, 4, 78, -25, 15, 0, 100, 124, 14, 8, 1, 2, 5, 15, 14, 33, 27, 35, 10, 24,
+                           32, 57, 51, 90, 1501, 12, 17, 151, 3, 44, 16, 16, -87, 65, 84, 45])
+    """
+    obj = MergeSorted((14, 33, 27, 10, 35, 19, 42, 44))
+    print(obj)
+
